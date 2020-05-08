41e7b99293a315dd97e4515820a42823a2494251
guixiaoyuan <463316523@qq.com>
2020-05-08 17:58:38 +0800

    add 刘海屏工具类

---
+++ NotchScreenUtils.java
@@ -0,0 +1,252 @@
+package com.lemeng.reader.lemengreader.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 刘海屏适配
+ *
+ * @author guixiaoyuan
+ */
+public class NotchScreenUtils {
+
+    /**
+     * 华为
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasNotchAtHuawei(Context context) {
+        boolean ret = false;
+        try {
+            ClassLoader cl = context.getClassLoader();
+            Class HwNotchSizeUtil = cl.loadClass("com.huawei.android.util.HwNotchSizeUtil");
+            Method get = HwNotchSizeUtil.getMethod("hasNotchInScreen");
+            ret = (boolean) get.invoke(HwNotchSizeUtil);
+        } catch (ClassNotFoundException e) {
+            Log.e("test", "hasNotchInScreen ClassNotFoundException");
+        } catch (NoSuchMethodException e) {
+            Log.e("test", "hasNotchInScreen NoSuchMethodException");
+        } catch (Exception e) {
+            Log.e("test", "hasNotchInScreen Exception");
+        } finally {
+            return ret;
+        }
+    }
+
+    //获取刘海尺寸：width、height
+    //int[0]值为刘海宽度 int[1]值为刘海高度
+    public static int[] getNotchSizeAtHuawei(Context context) {
+        int[] ret = new int[]{0, 0};
+        try {
+            ClassLoader cl = context.getClassLoader();
+            Class HwNotchSizeUtil = cl.loadClass("com.huawei.android.util.HwNotchSizeUtil");
+            Method get = HwNotchSizeUtil.getMethod("getNotchSize");
+            ret = (int[]) get.invoke(HwNotchSizeUtil);
+        } catch (ClassNotFoundException e) {
+            Log.e("Notch", "getNotchSizeAtHuawei ClassNotFoundException");
+        } catch (NoSuchMethodException e) {
+            Log.e("Notch", "getNotchSizeAtHuawei NoSuchMethodException");
+        } catch (Exception e) {
+            Log.e("Notch", "getNotchSizeAtHuawei Exception");
+        } finally {
+            return ret;
+        }
+    }
+
+    /**
+     * Vivo
+     */
+    public static final int VIVO_NOTCH = 0x00000020;//是否有刘海
+    public static final int VIVO_FILLET = 0x00000008;//是否有圆角
+
+    public static boolean hasNotchAtVivo(Context context) {
+        boolean ret = false;
+        try {
+            ClassLoader classLoader = context.getClassLoader();
+            Class FtFeature = classLoader.loadClass("android.util.FtFeature");
+            Method method = FtFeature.getMethod("isFeatureSupport", int.class);
+            ret = (boolean) method.invoke(FtFeature, VIVO_NOTCH);
+        } catch (ClassNotFoundException e) {
+            Log.e("Notch", "hasNotchAtVivo ClassNotFoundException");
+        } catch (NoSuchMethodException e) {
+            Log.e("Notch", "hasNotchAtVivo NoSuchMethodException");
+        } catch (Exception e) {
+            Log.e("Notch", "hasNotchAtVivo Exception");
+        } finally {
+            return ret;
+        }
+    }
+
+    /**
+     * OPPO
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasNotchAtOPPO(Context context) {
+        return context.getPackageManager().hasSystemFeature("com.oppo.feature.screen.heteromorphism");
+    }
+
+
+    /**
+     * 只适用于判断MIUI 是否有刘海屏
+     * SystemProperties.getInt("ro.miui.notch", 0) == 1;
+     *
+     * @return
+     */
+    public static boolean hasMIUINotchInScreen() {
+        try {
+            Class<?> clz = Class.forName("android.os.SystemProperties");
+            Method get = clz.getMethod("getInt", String.class, String.class);
+            return (int) get.invoke(clz, "ro.miui.notch", -1) == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 小米刘海屏判断.
+     *
+     * @return 0 if it is not notch ; return 1 means notch
+     * @throws IllegalArgumentException if the key exceeds 32 characters
+     */
+    public static int getInt(String key, Activity activity) {
+        int result = 0;
+        if (isXiaomi()) {
+            try {
+                ClassLoader classLoader = activity.getClassLoader();
+                @SuppressWarnings("rawtypes")
+                Class SystemProperties = classLoader.loadClass("android.os.SystemProperties");
+                //参数类型
+                @SuppressWarnings("rawtypes")
+                Class[] paramTypes = new Class[2];
+                paramTypes[0] = String.class;
+                paramTypes[1] = int.class;
+                Method getInt = SystemProperties.getMethod("getInt", paramTypes);
+                //参数
+                Object[] params = new Object[2];
+                params[0] = new String(key);
+                params[1] = new Integer(0);
+                result = (Integer) getInt.invoke(SystemProperties, params);
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 是否是小米手机
+     */
+    public static boolean isXiaomi() {
+        return "Xiaomi".equals(Build.MANUFACTURER);
+    }
+
+    //Xiaomi HUAWEI vivo OPPO
+    public static boolean isHUAWEI(){
+        return "HUAWEI".equals(Build.MANUFACTURER);
+    }
+
+    public static boolean isvivo(){
+        return "vivo".equals(Build.MANUFACTURER);
+    }
+
+    public static boolean isOPPO(){
+        return "OPPO".equals(Build.MANUFACTURER);
+    }
+
+    /**
+     * 获取MIUI 的刘海屏高度
+     *
+     * @param context
+     */
+    public static int getMIUINotchSize(Context context) {
+        int resourceId = context.getResources().getIdentifier("notch_height", "dimen", "android");
+        if (resourceId > 0) {
+            return context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return 0;
+    }
+
+    /**
+     * 小米
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusBarHeight(Context context) {
+        int statusBarHeight = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
+    }
+
+    /**
+     * 是否是刘海屏
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNotchScreen(Context context) {
+        if (NotchScreenUtils.hasNotchAtHuawei(context) ||
+                NotchScreenUtils.hasNotchAtOPPO(context) ||
+                NotchScreenUtils.hasNotchAtVivo(context) ||
+                NotchScreenUtils.hasMIUINotchInScreen()) {
+            return true;
+        }
+        if (NotchScreenUtils.getInt("ro.miui.notch", (Activity) context) == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 全屏
+     *
+     * @param activity
+     */
+    public static void fullScreen(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                //5.x开始需要把颜色设置透明，否则导航栏会呈现系统默认的浅灰色
+                Window window = activity.getWindow();
+                View decorView = window.getDecorView();
+                //两个 flag 要结合使用，表示让应用的主体内容占用系统状态栏的空间
+                int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                decorView.setSystemUiVisibility(option);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);
+            } else {
+                Window window = activity.getWindow();
+                WindowManager.LayoutParams attributes = window.getAttributes();
+                int flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+                int flagTranslucentNavigation = WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+                attributes.flags |= flagTranslucentStatus;
+                window.setAttributes(attributes);
+            }
+        }
+    }
+}
